e449b6a8e06f487c9f84717f11269f495e178b68
eedu7 <mueedahmad067@gmail.com>
2025-03-22 09:52:33 +0500

    FEAT: Add Prettier configuration and plugins for code formatting

--- frontend/src/app/(static_pages)/info/page.tsx
+++ frontend/src/app/(static_pages)/info/page.tsx
@@ -5,36 +5,38 @@ import React from "react";
 import AutumLeavesJPG from "@/constants/images";
 
 export const metadata: Metadata = {
-	title: "MyBlog: Read and write stories.",
+    title: "MyBlog: Read and write stories.",
 };
 
 const InfoPage = () => {
-	return (
-		<>
-			<div className="space-y-4 md:space-y-8">
-				<div>
-					<h1 className="text-4xl md:text-6xl font-semibold">Human</h1>
-					<h1 className="text-4xl md:text-6xl font-semibold">stories & ideas</h1>
-				</div>
-				<p className="text-lg text-gray-500">
-					A place to read, write, and deepen your understanding.
-				</p>
-				<button
-					type="button"
-					className="btn btn-outline">
-					Start reading
-				</button>
-			</div>
-			{/* TODO: Remove this when in smaller screen, show this in the tablets */}
-			<div className="hidden h-[44rem] lg:flex justify-end">
-				<Image
-					src={AutumLeavesJPG}
-					alt="Autum Leaves Image"
-					className="rounded shadow-lg object-cover"
-				/>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="space-y-4 md:space-y-8">
+                <div>
+                    <h1 className="text-4xl md:text-6xl font-semibold">
+                        Human
+                    </h1>
+                    <h1 className="text-4xl md:text-6xl font-semibold">
+                        stories & ideas
+                    </h1>
+                </div>
+                <p className="text-lg text-gray-500">
+                    A place to read, write, and deepen your understanding.
+                </p>
+                <button type="button" className="btn btn-outline">
+                    Start reading
+                </button>
+            </div>
+            {/* TODO: Remove this when in smaller screen, show this in the tablets */}
+            <div className="hidden h-[44rem] lg:flex justify-end">
+                <Image
+                    src={AutumLeavesJPG}
+                    alt="Autum Leaves Image"
+                    className="rounded shadow-lg object-cover"
+                />
+            </div>
+        </>
+    );
 };
 
 export default InfoPage;

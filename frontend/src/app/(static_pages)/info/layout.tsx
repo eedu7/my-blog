450dfac7828cd59bfa4f3280830521857fbb03c6
eedu7 <mueedahmad067@gmail.com>
2025-03-23 10:42:14 +0500

    feat: improve HomeLayout component styling and structure for better visual consistency

--- frontend/src/app/(static_pages)/info/layout.tsx
+++ frontend/src/app/(static_pages)/info/layout.tsx
@@ -4,24 +4,23 @@ import { InfoNavbar } from "@/modules/info/navbar";
 import { InfoPageFooter } from "@/modules/info/footer";
 
 interface HomeLayoutProps {
-	children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const HomeLayout = ({ children }: HomeLayoutProps) => {
-	return (
-		<div className="flex flex-col min-h-screen">
-			<header className="p-4">
-				<InfoNavbar />
-			</header>
-			<main className="flex-grow space-y-8 grid lg:grid-cols-2 justify-center items-center max-w-7xl mx-auto p-4">
-				{children}
-			</main>
-			<div className="divider h-0.5"></div>
-			<footer className="p-2">
-				<InfoPageFooter />
-			</footer>
-		</div>
-	);
+    return (
+        <div className="flex min-h-screen flex-col">
+            <header className="bg-gray-900 p-4 text-gray-200">
+                <InfoNavbar />
+            </header>
+            <main className="mx-auto grid max-w-7xl flex-grow items-center justify-center space-y-8 p-4 lg:grid-cols-2">
+                {children}
+            </main>
+            <footer className="bg-gray-900 p-4 text-gray-200">
+                <InfoPageFooter />
+            </footer>
+        </div>
+    );
 };
 
 export default HomeLayout;

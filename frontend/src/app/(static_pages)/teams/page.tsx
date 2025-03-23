ef89d118f4080b9ce75496e2c655c25e83a6a62d
eedu7 <mueedahmad067@gmail.com>
2025-03-23 15:17:25 +0500

    feat: add footer with links and branding to Teams page for improved navigation

--- frontend/src/app/(static_pages)/teams/page.tsx
+++ frontend/src/app/(static_pages)/teams/page.tsx
@@ -1,9 +1,11 @@
+import { INFO_LINK, MembershipPageFooterLinks } from "@/constants/navbars";
 import { GetInTouchSection } from "@/modules/teams/get-in-touch";
 import { TeamsPageHeader } from "@/modules/teams/header";
 import { TeamsHeroSection } from "@/modules/teams/hero";
 import { TeamsInfoSection } from "@/modules/teams/teams-info";
 import { TeamsMemberSection } from "@/modules/teams/teams-members";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "Medium for Teams",
@@ -19,6 +21,20 @@ const TeamsPage = () => {
                 <TeamsInfoSection />
                 <GetInTouchSection />
             </main>
+            <footer className="flex flex-col items-center justify-between space-y-2 bg-gray-800 p-2 text-gray-200 md:flex-row">
+                <Link href={INFO_LINK.href}>
+                    <h1 className="text-2xl md:text-4xl">MyBlog</h1>
+                </Link>
+                <ul className="flex gap-2 text-xs md:text-sm">
+                    {MembershipPageFooterLinks.map(({ id, title, href }) => (
+                        <li key={id}>
+                            <Link href={href} className="link underline-offset-2">
+                                {title}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </footer>
         </>
     );
 };
